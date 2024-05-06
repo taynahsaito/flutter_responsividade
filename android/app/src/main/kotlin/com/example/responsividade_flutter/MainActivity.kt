1018c06f91bf2fa4383847f1dea1c2875990a468
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-05-06 14:43:44 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/responsividade_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.responsividade_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:responsividade_flutter/app.dart';
+
+void main() {
+  runApp(const App());
+}
